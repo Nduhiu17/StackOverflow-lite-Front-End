078d615143c109c3f93362dc7edc34e2df4f88b4
Your Name <nduhiu254@gmail.com>
2018-09-20 12:53:05 +0300

    [Chore #[160648541]] Remove unneccesary codes that were committed on the wrong branch

--- UI/js/question-detail.js
+++ UI/js/question-detail.js
@@ -1,7 +1,6 @@
 const id = window.location.search.slice(1);
 const el = document.querySelector("#question-details");
 const answers = document.querySelector("#answers");
-const deleteBtn = document.querySelector("#delete-question-button");
 
 fetch(`http://127.0.0.1:5000/api/v1/questions/${id}`)
   .then(res => res.json())
@@ -38,20 +37,3 @@ fetch(`http://127.0.0.1:5000/api/v1/questions/${id}`)
   });
 
 
-deleteBtn.addEventListener("click", () => {
-  return fetch(`http://127.0.0.1:5000/api/v1/questions/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`
-    }
-  })
-    .then(res => res.json())
-    .then(data => {
-        if(data.message === 'Successfully deleted') {
-            window.location.replace('home.html')
-        } else {
-            alert('You are not authorized to delete the question')
-        }
-    });
-});
